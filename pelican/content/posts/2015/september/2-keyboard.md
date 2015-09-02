4344f8065bcf9e4f3669f3d56582744412e29476
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-09-02 11:47:10 -0600

    initial commit of pelican code

---
+++ pelican/content/posts/2015/september/2-keyboard.md
@@ -0,0 +1,9 @@
+Title: My super title
+Date: 2015-9-02
+Category: Python
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: Alexis Metaireau, Conan Doyle
+Summary: Short version for index and feeds
+
+This will be an awesome post.
---
+++ pelican/pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Pedro Rodriguez'
+SITENAME = u'Pedro Rodriguez'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Denver'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS ='posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_URL = '/{slug}'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'theme'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
