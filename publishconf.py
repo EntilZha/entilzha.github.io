e47544ce46b774a7ae2cfe4ec1187be97d2b25cf
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-07-23 07:19:03 -0600

    Update website to use SSL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://entilzha.github.io'
+SITEURL = 'https://entilzha.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
