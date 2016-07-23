9b3e6b11029ac19a10d071806d60920acc44c72b
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-07-23 07:35:06 -0600

    publish script

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+pelican content -o output -s publishconf.py
+ghp-import -b master -m 'Updated website' output
+
+echo "Now run git checkout master, git push origin master, git checkout source"
