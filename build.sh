88350ccbc873651cd0a323ffa1c14df1d65c0c07
EntilZha <pedro@entilzha.io>
2019-09-24 23:36:32 -0700

    netlify test

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt update
+apt install -y git
+apt install -y nodejs npm
+npm install -g uglify-js
+npm install -g babel-cli@6 babel-preset-react-app@3
+pip install -r requirements.txt
+export PYTHONPATH="${PYTHONPATH}:/pelican-plugins"
+git clone https://github.com/getpelican/pelican-plugins.git pelican-plugins
+cd pelican-plugins
+git checkout 0b9e66ee
+git submodule update --recursive
+cd ..
+
+pelican content -o output -s publishconf.py
