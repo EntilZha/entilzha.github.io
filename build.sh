8e06537a16d156f2f42e0946c21d9e1750453e31
EntilZha <pedro@entilzha.io>
2019-09-24 23:40:20 -0700

    Retry path

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ apt install -y nodejs npm
 npm install -g uglify-js
 npm install -g babel-cli@6 babel-preset-react-app@3
 pip install -r requirements.txt
-export PYTHONPATH="${PYTHONPATH}:/pelican-plugins"
 git clone https://github.com/getpelican/pelican-plugins.git pelican-plugins
 cd pelican-plugins
 git checkout 0b9e66ee
 git submodule update --recursive
 cd ..
+export PYTHONPATH="${PYTHONPATH}:pelican-plugins"
 
 pelican content -o output -s publishconf.py
