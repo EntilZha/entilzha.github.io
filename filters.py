61ff4857755d6c022e2e66f29f1412400d4c9161
Pedro Rodriguez <pedro@entilzha.io>
2020-04-11 18:14:17 -0400

    Update filters.py

--- filters.py
+++ filters.py
@@ -11,10 +11,12 @@ def news(_):
     import markdown
     html = markdown.markdown(
 """
+* [Blog post on AllenNLP Callbacks and CometML](https://www.pedro.ai/blog/2020/04/08/allennlp-callback-trainer-cometml/)
 * [Blog post on Reproducible ML](/blog/2020/03/24/reproducible-ml-and-parameter-sweeps/)
 * [Blog post on Debugging ML Code](/blog/2019/12/15/debug-ml-code/)
 * Finished [Google AI](https://ai.google/research/)-Zurich winter research internship!
 * Finished [Facebook Conversational AI](https://ai.facebook.com/) summer research internship
+* [arXiv Preprint for Quizbowl](https://arxiv.org/abs/1904.04792)
 * [TACL Paper](https://www.pedro.ai/static/publications/2019_tacl_trick.pdf) on Adversarial Question Generation
 """)
     return html
