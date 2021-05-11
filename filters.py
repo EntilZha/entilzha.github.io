1a09c3280fc608d23ee9767d1158e4769534baf6
Pedro Rodriguez <me@pedro.ai>
2021-05-11 16:18:32 -0700

    update newsbar

--- filters.py
+++ filters.py
@@ -11,13 +11,12 @@ def news(_):
     import markdown
     html = markdown.markdown(
 """
-* Defending PhD Thesis on "Evaluating Machine Intelligence with Question Answering" on April 9th
+* [ACL 2021 Acceptance for Paper on Item Response Theory for NLP Leaderboards](https://twitter.com/EntilZhaPR/status/1390329941447778304)
+* Defended PhD Thesis on "Evaluating Machine Intelligence with Question Answering" on April 9th
 * [EMNLP 2020 Paper from FB Internship](https://www.aclweb.org/anthology/2020.emnlp-main.655/)
 * [Blog post on AllenNLP Callbacks and CometML](https://www.pedro.ai/blog/2020/04/08/allennlp-callback-trainer-cometml/)
 * [Blog post on Reproducible ML](/blog/2020/03/24/reproducible-ml-and-parameter-sweeps/)
 * [Blog post on Debugging ML Code](/blog/2019/12/15/debug-ml-code/)
-* Finished [Google AI](https://ai.google/research/)-Zurich winter research internship!
-* Finished [Facebook Conversational AI](https://ai.facebook.com/) summer research internship
 * [arXiv Preprint for Quizbowl](https://arxiv.org/abs/1904.04792)
 * [TACL Paper](https://www.pedro.ai/static/publications/2019_tacl_trick.pdf) on Adversarial Question Generation
 """)
