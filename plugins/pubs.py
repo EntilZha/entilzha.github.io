02db90879c76aba5531e0764c1eb2142f826a260
Pedro Rodriguez <me@pedro.ai>
2022-05-26 14:48:33 -0700

    Update pubs

--- plugins/pubs.py
+++ plugins/pubs.py
@@ -19,7 +19,7 @@ template = """
 </li>
 {% endfor %}
 </ul>
-<h3>Refereed</h3>
+<h3>Conference and Journal Papers</h3>
 <ul>
 {% for e in publications %}
 <li>
@@ -35,7 +35,7 @@ template = """
 </li>
 {% endfor %}
 </ul>
-<h3>Workshops</h3>
+<h3>Workshop Papers</h3>
 <ul>
 {% for e in workshops %}
 <li>
@@ -73,8 +73,8 @@ template = """
 
 def author_format(author):
     author = author.strip()
-    if author.lower() == 'pedro rodriguez':
-        return '<b>' + author + '</b>'
+    if author.lower() == "pedro rodriguez":
+        return "<b>" + author + "</b>"
     else:
         return author
 
@@ -85,29 +85,27 @@ def comma_join(authors):
     elif len(authors) == 1:
         return authors[0]
     else:
-        return ', '.join(authors[:-1]) + ", and " + authors[-1]
-
+        return ", ".join(authors[:-1]) + ", and " + authors[-1]
 
 
 def extract_citation(entry):
     entry = copy.deepcopy(entry)
-    del entry['type']
+    del entry["type"]
     single_entry_db = BibDatabase()
     single_entry_db.entries = [entry]
     writer = BibTexWriter()
     return bibtexparser.dumps(single_entry_db, writer).strip()
 
 
-
 class PublicationsReader(BaseReader):
     enabled = True
-    file_extensions = ['bib']
+    file_extensions = ["bib"]
 
     def read(self, filename):
         metadata = {
-            'title': 'Publications',
-            'category': 'Publications',
-            'date': str(datetime.datetime.now())
+            "title": "Publications",
+            "category": "Publications",
+            "date": str(datetime.datetime.now()),
         }
         parsed = {}
         for key, value in metadata.items():
@@ -125,18 +123,18 @@ class PublicationsReader(BaseReader):
             non_refereed = []
             media = []
             projects = []
-            
-            thesis = [e for e in entries if e['type'] == 'thesis']
-            publications = [e for e in entries if e['type'] == 'publication']
-            arxiv = [e for e in entries if e['type'] == 'arxiv']
-            workshops = [e for e in entries if e['type'] == 'workshop']
-            non_refereed = [e for e in entries if e['type'] == 'non-refereed']
-            media = [e for e in entries if e['type'] == 'media']
-            projects = [e for e in entries if e['type'] == 'project']
+
+            thesis = [e for e in entries if e["type"] == "thesis"]
+            publications = [e for e in entries if e["type"] == "publication"]
+            arxiv = [e for e in entries if e["type"] == "arxiv"]
+            workshops = [e for e in entries if e["type"] == "workshop"]
+            non_refereed = [e for e in entries if e["type"] == "non-refereed"]
+            media = [e for e in entries if e["type"] == "media"]
+            projects = [e for e in entries if e["type"] == "project"]
             for e in entries:
-                del e['type']
+                del e["type"]
             jinja_env = Environment()
-            jinja_env.filters['compile_jsx'] = compile_jsx
+            jinja_env.filters["compile_jsx"] = compile_jsx
             html = jinja_env.from_string(template).render(
                 thesis=thesis,
                 publications=publications,
@@ -144,41 +142,42 @@ class PublicationsReader(BaseReader):
                 arxiv=arxiv,
                 non_refereed=non_refereed,
                 projects=projects,
-                workshops=workshops
+                workshops=workshops,
             )
         return html, parsed
 
     def _parse_entry(self, entry):
-        authors = [author_format(e) for e in entry['author'].split(' and ')]
-        if 'booktitle' in entry:
-            source = entry['booktitle']
-            volume = ''
-        elif 'journal' in entry:
-            source = entry['journal']
-            volume = entry.get('volume', '')
-        elif 'school' in entry:
-            source = entry['school']
-            volume = ''
+        authors = [author_format(e) for e in entry["author"].split(" and ")]
+        if "booktitle" in entry:
+            source = entry["booktitle"]
+            volume = ""
+        elif "journal" in entry:
+            source = entry["journal"]
+            volume = entry.get("volume", "")
+        elif "school" in entry:
+            source = entry["school"]
+            volume = ""
         else:
-            source = ''
-            volume = ''
+            source = ""
+            volume = ""
         parsed_entry = {
-            'authors': comma_join(authors),
-            'bib_authors': entry['author'],
-            'url': entry['url'],
-            'title': entry['title'].replace('{', '').replace('}', ''),
-            'year': entry['year'],
-            'source': source,
-            'volume': volume,
-            'type': entry['type'],
-            'id': '@' + entry['ID']
+            "authors": comma_join(authors),
+            "bib_authors": entry["author"],
+            "url": entry["url"],
+            "title": entry["title"].replace("{", "").replace("}", ""),
+            "year": entry["year"],
+            "source": source,
+            "volume": volume,
+            "type": entry["type"],
+            "id": "@" + entry["ID"],
         }
         cite = extract_citation(entry)
-        parsed_entry['cite'] = cite
+        parsed_entry["cite"] = cite
         return parsed_entry
 
+
 def add_reader(readers):
-    readers.reader_classes['pubs'] = PublicationsReader
+    readers.reader_classes["pubs"] = PublicationsReader
 
 
 def register():
